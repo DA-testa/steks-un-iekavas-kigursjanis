816832de26d0f91df0f1d8088a76c9bf0366b57e
kigursjanis <125827153+kigursjanis@users.noreply.github.com>
2023-02-19 22:06:29 +0200

    Add files via upload

---
+++ LD_JANISKIGURS_221RDB120.py
@@ -0,0 +1,34 @@
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i+1))
+        elif next in ")]}":
+            if not opening_brackets_stack:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
+
+
+def main():
+    text = input("Input: ")
+    mismatch = find_mismatch(text)
+    print(mismatch)
+
+
+if __name__ == "__main__":
+    main()
